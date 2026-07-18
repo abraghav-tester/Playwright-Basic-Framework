4390ff2d8e1b6408d42daef01b61525f0694d860
Raghav <abhishek.raghav@capgemini.com>
2026-07-18 23:48:42 +0530

    config updated

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
  reporter: [
-    ['html'],
+    ['html'],['github'],
     ['allure-playwright']
   ],
 
